eb55221729047bda9a90b80aa53d1243f4e89888
ErmineNotyours <arthurallen@centurylink.net>
2017-10-14 17:20:05 -0700

    Formatted Lab 7 table

--- appSales2.js
+++ appSales2.js
@@ -1,6 +1,6 @@
 'use strict';
 console.log('Top of script.');
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 // get the reference for the body
 var body = document.getElementsByTagName('body')[0];
 
@@ -24,8 +24,8 @@ function Store (name, minCustPerHour, maxCustPerHour, avgCookeSoldPerHour, calcR
   };
   this.calcCookiesSoldByHour = function() {
     for(var j = 0; j < hours.length; j++) {
-      this.cookiesSoldByHour.push(Math.floor((Math.random() * this.avgCookeSoldPerHour * this.randCustByHour[j])));
-      console.log('j, this.randCustByHour, this.cookiesSoldByHour: ' + j + ' ' + this.randCustByHour[j] + ' ' + this.cookiesSoldByHour[j]);
+      this.cookiesSoldByHour.push(Math.floor(this.avgCookeSoldPerHour * this.randCustByHour[j]));
+      console.log('j, this.randCustByHour, this.avgCookeSoldPerHour, this.cookiesSoldByHour: ' + j + ' ' + this.randCustByHour[j] + ' ' + this.avgCookeSoldPerHour + ' ' + this.cookiesSoldByHour[j]);
     }
   };
   this.render = function() {
